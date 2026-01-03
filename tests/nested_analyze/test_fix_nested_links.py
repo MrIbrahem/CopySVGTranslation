0275168efb3eacdaa21432faae3ebedbc82b99b4
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-01-03 07:14:00 +0300

    Update test_fix_nested_links.py

--- tests/nested_analyze/test_fix_nested_links.py
+++ tests/nested_analyze/test_fix_nested_links.py
@@ -220,3 +220,39 @@ def test_match_and_fix_2(temp_dir: Path):
     new_text_expected_strip = "".join(new_text_expected.split())
 
     assert new_text_strip == new_text_expected_strip
+
+
+def test_match_and_fix_3(temp_dir: Path):
+    text = '''
+        <g class="markdown-text-wrap">
+            <text x="10.0" y="94.3" style="font-size: 12.375px; fill: rgb(133, 133, 133); line-height: 1.2;">
+                <tspan x="10" y="124.0">
+                    Read more: <a href="https://ourworldindata.org/age-standardization" target="_blank" rel="noopener" style="text-decoration: underline;">
+                    How does age standardization make health metrics comparable?
+                    </a>
+                </tspan>
+            </text>
+        </g>
+
+    '''
+    p = _write_svg(
+        temp_dir,
+        text,
+        name="testx.svg"
+    )
+    before = len(match_nested_tags(p))
+    fixed = fix_nested_file(p)
+    assert fixed is True
+
+    after = len(match_nested_tags(p))
+    assert before == 1
+    assert after == 0
+
+    new_text = p.read_text(encoding="utf-8")
+    new_text_expected = '''
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100"><g class="markdown-text-wrap"><text x="10.0" y="94.3" style="font-size: 12.375px; fill: rgb(133, 133, 133); line-height: 1.2;"><tspan x="10" y="124.0">Read more:How does age standardization make health metrics comparable?</tspan></text></g></svg>
+    '''
+    new_text_strip = "".join([line.strip() for line in new_text.splitlines() if line.strip()])
+    new_text_expected_strip = "".join([line.strip() for line in new_text_expected.splitlines() if line.strip()])
+
+    assert new_text_strip == new_text_expected_strip
