10ce12d06b39e9de3bff9492d4cbcad82897c993
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-02 02:39:33 +0300

    .

--- tests/e2e/svg_translate/test_svgtranslate.py
+++ tests/e2e/svg_translate/test_svgtranslate.py
@@ -15,11 +15,11 @@ from CopySVGTranslation.injection import generate_unique_id, inject
 from CopySVGTranslation.text_utils import normalize_text
 
 
-class TestSVGTranslate:
+class TestSetup:
     """Test cases for the SVG translation tool."""
 
     @pytest.fixture(autouse=True)
-    def setUp(self):
+    def setUp(self, mock_arabic_svg_content, mock_no_translations_svg_content):
         """
         Prepare temporary directory and SVG test fixtures used by the test cases.
 
@@ -31,57 +31,9 @@ class TestSVGTranslate:
             expected_translations: Mapping structure representing expected translation mappings for the two English source strings to Arabic.
         """
         self.test_dir = Path(tempfile.mkdtemp())
-        self.arabic_svg_content = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="1000" height="1000" id="svg2235">
-    <g id="foreground">
-        <switch style="font-size:30px;font-family:Bitstream Vera Sans">
-            <text x="250.88867" y="847.29651" style="font-size:30px;font-family:Bitstream Vera Sans"
-                id="text2205-ar"
-                xml:space="preserve" systemLanguage="ar">
-                <tspan x="250.88867" y="847.29651" id="tspan2207-ar">السماعات الخلفية تنقل الإشارة نفسها،</tspan>
-            </text>
-            <text x="250.88867" y="847.29651" style="font-size:30px;font-family:Bitstream Vera Sans"
-                id="text2205"
-                xml:space="preserve">
-                <tspan x="250.88867" y="847.29651" id="tspan2207">Rear speakers carry same signal,</tspan>
-            </text>
-        </switch>
-        <switch style="font-size:30px;font-family:Bitstream Vera Sans">
-            <text x="259.34814" y="927.29651" style="font-size:30px;font-family:Bitstream Vera Sans"
-                id="text2213-ar"
-                xml:space="preserve" systemLanguage="ar">
-                <tspan x="259.34814" y="927.29651" id="tspan2215-ar">لكنها موصولة بمرحلتين متعاكستين.</tspan>
-            </text>
-            <text x="259.34814" y="927.29651" style="font-size:30px;font-family:Bitstream Vera Sans"
-                id="text2213"
-                xml:space="preserve">
-                <tspan x="259.34814" y="927.29651" id="tspan2215">but are connected in anti-phase</tspan>
-            </text>
-        </switch>
-    </g>
-</svg>"""
+        self.arabic_svg_content = mock_arabic_svg_content
 
-        self.no_translations_svg_content = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" width="1000" height="1000" id="svg2235">
-    <g id="foreground">
-        <switch style="font-size:30px;font-family:Bitstream Vera Sans">
-            <text x="250.88867" y="847.29651" style="font-size:30px;font-family:Bitstream Vera Sans"
-                id="text2205"
-                xml:space="preserve">
-                <tspan x="250.88867" y="847.29651" id="tspan2207">Rear speakers carry same signal,</tspan>
-            </text>
-        </switch>
-        <switch style="font-size:30px;font-family:Bitstream Vera Sans">
-            <text x="259.34814" y="927.29651" style="font-size:30px;font-family:Bitstream Vera Sans"
-                id="text2213"
-                xml:space="preserve">
-                <tspan x="259.34814" y="927.29651" id="tspan2215">but are connected in anti-phase</tspan>
-            </text>
-        </switch>
-    </g>
-</svg>"""
+        self.no_translations_svg_content = mock_no_translations_svg_content
 
         self.expected_arabic_texts = [
             "السماعات الخلفية تنقل الإشارة نفسها،",
@@ -114,6 +66,7 @@ class TestSVGTranslate:
         for expected in texts_to_check:
             assert expected in found_texts
 
+class TestSVGTranslate(TestSetup):
     def test_normalize_text(self):
         """Test text normalization."""
         assert normalize_text("  hello  world  ") == "hello world"
