b3deb8c4af08898efcc49d5077d46aade643e668
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-03 04:30:03 +0300

    .

---
+++ tests/unit/extraction/test_extraction_worker.py
@@ -0,0 +1,13 @@
+# ruff: noqa: F401
+"""
+Unit tests for CopySVGTranslation/CopySVGTranslation/extraction/worker.py module.
+
+Functions to test: extract
+
+TODO: write tests
+"""
+
+
+from CopySVGTranslation.extraction.worker import (
+    extract,
+)
