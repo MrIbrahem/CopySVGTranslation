304ee2879f61869f8c0d709534e9f4b375e6bf74
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-08-05 02:02:58 +0300

    Update __init__.py

--- CopySVGTranslation/nested_analyze/__init__.py
+++ CopySVGTranslation/nested_analyze/__init__.py
@@ -3,9 +3,12 @@
 from .find_nested import fix_nested_file
 from .match_tags import match_nested_tags
 
+from .detector import NestedTspanDetector
 from .flattener import NestedTspanFlattener
 
 __all__ = [
+    "NestedTspanDetector",
+    "NestedTspanFlattener",
     "fix_nested_file",
     "match_nested_tags",
 ]
